1174414cd6da2955bb868ce29cae3196fa5e6bd1
Abderrahman El haddad asofi <ael-hadd@e1r13p4.1337.ma>
2023-02-23 11:37:22 +0100

    Adding blog files

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import BlogDetails from './BlogDetails';
+import Create from './Create';
+import Home from './Home';
+import NavBar from './NavBar';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import NotFound from './NotFound';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <NavBar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/create">
+              <Create />
+            </Route>
+            <Route path="/blog/:id">
+              <BlogDetails />
+            </Route>
+            <Route path="*">
+              <NotFound />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/NavBar.js
@@ -0,0 +1,24 @@
+import { Link } from 'react-router-dom';
+
+const NavBar = () => {
+  return (
+    <nav className="navbar">
+      <h1>My Blog</h1>
+      <div className="links">
+        <Link to="/">Home</Link>
+        <Link
+          to="/create"
+          style={{
+            color: 'white',
+            backgroundColor: '#f1356d',
+            borderRadius: '8px',
+          }}
+        >
+          New Blog
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
